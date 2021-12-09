07a7ef7deef34f369fa99aefd64719c493e999ef
Abe Voelker <_@abevoelker.com>
2021-12-09 12:22:18 -0600

    Complete day 5 part 2

--- day5/Main.hs
+++ day5/Main.hs
@@ -5,7 +5,7 @@ import Data.Map (toList, fromListWith)
 -- Part 1
 
 parseLine :: String -> ((Int, Int), (Int, Int))
-parseLine x = ((y !! 0, y !! 1), (y !! 2, y !! 3)) where --(pairs nums !! 0, pairs nums !! 1) where
+parseLine x = ((y !! 0, y !! 1), (y !! 2, y !! 3)) where
   y = map (\x -> read x::Int) $ concat $ map (splitOn ",") $ (splitOn " -> " x)
 
 pointsBetween :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
@@ -14,10 +14,25 @@ pointsBetween (x,y) (x',y')
                                    j <- [(min y y')..(max y y')] ]
   | otherwise           = []
 
+-- Part 2
+
+pointsBetween' :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+pointsBetween' (x,y) (x',y')
+  -- horizontal/vertical line
+  | x == x' || y == y'  = [(i,j) | i <- [(min x x')..(max x x')],
+                                   j <- [(min y y')..(max y y')] ]
+  -- diagonal line
+  | otherwise           = zip (diag x x') (diag y y')
+  where
+    diag x y = if (x < y) then (enumFromTo x y) else reverse (enumFromTo y x)
+
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
   let l = map parseLine (lines contents)
   let allPoints = concat $ map (\x -> pointsBetween (fst x) (snd x)) l
+  let allPoints' = concat $ map (\x -> pointsBetween' (fst x) (snd x)) l
   let pointCounts = toList $ fromListWith (+) [(c, 1) | c <- allPoints]
-  print $ length $ filter (\(_,x) -> x >= 2) pointCounts
+  let pointCounts' = toList $ fromListWith (+) [(c, 1) | c <- allPoints']
+  putStrLn $ "Part 1: " ++ (show $ length $ filter (\(_,x) -> x >= 2) pointCounts)
+  putStrLn $ "Part 2: " ++ (show $ length $ filter (\(_,x) -> x >= 2) pointCounts')
