7ccc8a2c6a1320f1c5381c47b252c20b000bea5c
Abe Voelker <_@abevoelker.com>
2021-12-09 09:24:00 -0600

    Add part 2 to day 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,8 +1,18 @@
+import Data.List (transpose)
+
 depthIncreaseCount :: [Int] -> Int
 depthIncreaseCount xs = length $ filter (\x -> snd x > fst x ) (pairs xs) where
   pairs [] = []
   pairs xs = zip xs (tail xs)
 
-main = do
+part1 = do
   contents <- readFile "input.txt"
-  return $ depthIncreaseCount $ map (\x -> read x::Int) (lines contents)
+  let depths = map (\x -> read x::Int) (lines contents)
+  return $ depthIncreaseCount depths
+
+part2 = do
+  contents <- readFile "input.txt"
+  let depths = map (\x -> read x::Int) (lines contents)
+  let slidingWindow = transpose [depths, (drop 1 depths), (drop 2 depths)]
+  let slidingWindowSums = map sum $ filter (\x -> length x == 3) slidingWindow
+  return $ depthIncreaseCount slidingWindowSums
